8604d2f427695b04eac0a5c0aa34be915b5dcd18
PAT <tuanbacyen@gmail.com>
2020-04-02 17:16:45 +0700

    render board with double loop

--- src/index.js
+++ src/index.js
@@ -21,24 +21,17 @@ class Board extends React.Component {
   }
 
   render() {
+    let listBoard = [];
+    for (let i = 0; i < 3; i++) {
+      let listSquare = [];
+      for (let j = 0; j < 3; j++) {
+        listSquare.push(this.renderSquare(j + i * 3))
+      }
+      listBoard.push(<div className="board-row">{listSquare}</div>);
+    }
+
     return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
+      <div>{listBoard}</div>
     );
   }
 }
